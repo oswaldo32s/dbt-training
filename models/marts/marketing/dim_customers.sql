ceb11810885c37870c5aa72aac3d0dec069556f8
Oswaldo Gonzalez <oswaldo32dev@gmail.com>
2025-04-21 17:07:52 +0000

    Add staging tables and dim_customers

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,37 @@
+with customers as (
+    select
+        customer_id,
+        first_name,
+        last_name
+    from {{ ref('stg_jaffle_shop__customers') }}
+),
+
+orders as (
+    select
+        order_id,
+        customer_id,
+        order_date,
+        order_status
+    from {{ ref('stg_jaffle_shop__orders') }}
+),
+
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    from orders
+    group by 1
+)
+
+select
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    co.first_order_date,
+    co.most_recent_order_date,
+    coalesce(co.number_of_orders, 0)
+from customers as c
+left join customer_orders as co
+    on c.customer_id = co.customer_id
