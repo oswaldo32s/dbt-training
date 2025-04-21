86b420709cf0849190c6783ea7594e064a21d0f5
Oswaldo Gonzalez <oswaldo32dev@gmail.com>
2025-04-21 17:37:12 +0000

    create fact_orders and update dim_customers

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -11,8 +11,8 @@ orders as (
         order_id,
         customer_id,
         order_date,
-        order_status
-    from {{ ref('stg_jaffle_shop__orders') }}
+        amount
+    from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -20,7 +20,8 @@ customer_orders as (
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
     group by 1
 )
@@ -31,7 +32,8 @@ select
     c.last_name,
     co.first_order_date,
     co.most_recent_order_date,
-    coalesce(co.number_of_orders, 0)
+    coalesce(co.number_of_orders, 0) as number_of_orders,
+    coalesce(co.lifetime_value, 0) as lifetime_value
 from customers as c
 left join customer_orders as co
     on c.customer_id = co.customer_id
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,8 @@
+select
+    id as payment_id,
+    orderId as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount / 100 as amount,
+    created as created_date
+from default.stripe_payments
